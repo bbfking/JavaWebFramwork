1078432f69193bbe030566543c18b036ac2f7590
pfxiong <bbfking@gmail.com>
2015-11-10 23:11:29 +0800

    test for git

--- src/main/java/com/pfxiong/chapter1/HelloServlet.java
+++ src/main/java/com/pfxiong/chapter1/HelloServlet.java
@@ -14,6 +14,7 @@ import java.util.Date;
 public class HelloServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //test for git
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
         req.setAttribute("currentTime", currentTime);
